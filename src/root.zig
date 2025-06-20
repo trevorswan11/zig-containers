21508b89f63d063f8c26bd6c03a116d5378cd799
trevorswan11 <tcs94@case.edu>
2025-06-20 19:45:14 -0400

    added adjacency list and matrix to the root

--- src/root.zig
+++ src/root.zig
@@ -10,6 +10,8 @@ pub const PQType = @import("dsa/priority_queue.zig").PQType;
 pub const HashMap = @import("dsa/hash_map.zig").HashMap;
 pub const HashSet = @import("dsa/hash_set.zig").HashSet;
 pub const Graph = @import("dsa/graph.zig").Graph;
+pub const AdjacencyList = @import("dsa/adjacency_list.zig").AdjacencyList;
+pub const AdjacencyMatrix = @import("dsa/adjacency_matrix.zig").AdjacencyMatrix;
 
 // Memory
 pub const Ref = @import("mem/shared_ptr.zig").SharedPtr;
@@ -26,6 +28,8 @@ test {
     _ = @import("dsa/hash_map.zig");
     _ = @import("dsa/hash_set.zig");
     _ = @import("dsa/graph.zig");
+    _ = @import("dsa/adjacency_list.zig");
+    _ = @import("dsa/adjacency_matrix.zig");
 
     _ = @import("mem/shared_ptr.zig");
     _ = @import("mem/unique_ptr.zig");
